af496a9d849966ce53e2500a554f5b6c85c31be0
andreychvankov <whitebear134@gmail.com>
2025-12-05 02:46:47 +0300

    streaming saving to file

--- vstream_service/app/utils/frame_collector.py
+++ vstream_service/app/utils/frame_collector.py
@@ -1,4 +1,6 @@
 import os
+from fractions import Fraction
+
 import av
 import asyncio
 from uuid import UUID
@@ -13,16 +15,31 @@ logger = get_logger(__name__)
 class FrameCollector(BaseFrameCollector):
     def __init__(self, session_id: UUID):
         super().__init__()
-        self.session_id = session_id
-        self.frames = []
+        self._session_id = session_id
 
         self._temp_dir = f"/tmp/collected_data/"
         os.makedirs(self._temp_dir, exist_ok=True)
-        self._file_name = f"{session_id}.mp4"
+        self._file_name = f"{self._session_id}.mp4"
         self._output_file_path = os.path.join(self._temp_dir, self._file_name)
 
         self._lock = asyncio.Lock()
         self._metadata = None
+
+        self._container = av.open(self._output_file_path, mode="w")
+        self._stream = None
+        self._frame_index = None
+
+        self.FPS = 30
+        self.CODEC = "libx264"
+        self.BIT_RATE = 5_000_000
+        self.MIME_TYPE = "video/mp4"
+        self.PIXEL_FORMAT = "yuv420p"
+        self.OPTIONS = {
+            "crf": "23",
+            "preset": "fast",
+            "tune": "zerolatency"
+        }
+
 
     @property
     def file_name(self):
@@ -35,71 +52,73 @@ class FrameCollector(BaseFrameCollector):
     def file_exists(self):
         return os.path.exists(self._output_file_path)
 
-    async def add_frame(self, frame):
-        try:
-            async with self._lock:
-                self.frames.append(frame.to_ndarray(format="bgr24"))
-        except Exception as e:
-            logger.error(f"session: {self.session_id} - Error adding frame in session: {e}")
+    async def add_frame(self, frame: av.VideoFrame):
+        async with self._lock:
+            try:
+                if self._stream is None:
+                    self._stream = self._container.add_stream(self.CODEC, rate=self.FPS, options=self.OPTIONS)
+                    self._stream.width = frame.width
+                    self._stream.height = frame.height
+                    self._stream.pix_fmt = self.PIXEL_FORMAT
+                    self._stream.time_base = Fraction(1, self.FPS)
+                    self._stream.bit_rate = self.BIT_RATE
+                    self._frame_index = 0
+
+                frame.pts = self._frame_index
+                frame.time_base = Fraction(1, self.FPS)
+                self._frame_index += 1
+
+                for packet in self._stream.encode(frame):
+                    self._container.mux(packet)
+
+            except Exception as e:
+                logger.error(f"session: {self._session_id} - error adding frame in session: {e}")
 
     async def finalize(self):
-        logger.info(f"session: {self.session_id} - We are starting finalization of frames={len(self.frames)}")
-        if not self.frames:
-            logger.warning(f"session: {self.session_id} - There are no frames to save.")
-            return None
-
-        logger.info(f"session: {self.session_id} - Save {len(self.frames)} frames to {self._output_file_path}")
+        logger.info(f"session: {self._session_id} - finalize video data to {self._output_file_path}")
         try:
-            container = av.open(self._output_file_path, mode="w")
-            stream = container.add_stream("libx264", rate=30)
-            stream.pix_fmt = "yuv420p"
-            stream.width = self.frames[0].shape[1]
-            stream.height = self.frames[0].shape[0]
+            for packet in self._stream.encode():
+                self._container.mux(packet)
 
-            for frame_array in self.frames:
-                frame = av.VideoFrame.from_ndarray(frame_array, format="bgr24")
-                packet = stream.encode(frame)
-                if packet:
-                    container.mux(packet)
-
-            for packet in stream.encode(None):
-                container.mux(packet)
-
-            container.close()
-
-            logger.info(f"session: {self.session_id} - the video is saved locally: {self._output_file_path}")
+            logger.info(f"session: {self._session_id} - the video is saved locally: {self._output_file_path}")
         except Exception as e:
-            logger.error(f"session: {self.session_id} - error while compiling video: {e}")
+            logger.error(f"session: {self._session_id} - error while compiling video: {e}")
+        finally:
+            self._container.close()
+        try:
+            logger.info(f"session: {self._session_id} - getting metadata...")
+            await self.get_metadata()
+        except Exception as e:
+            logger.error(f"session: {self._session_id} - error getting metadata: {e}")
 
-        return self._output_file_path
+        return self._output_file_path, self._metadata
 
     async def cleanup(self):
-        logger.info(f"session: {self.session_id} - collector cleaning up")
+        logger.info(f"session: {self._session_id} - collector cleaning up")
         try:
             if os.path.exists(self._output_file_path):
                 os.remove(self._output_file_path)
-            logger.info(f"session: {self.session_id} - temporary file removed")
+            logger.info(f"session: {self._session_id} - temporary file removed")
         except Exception as e:
-            logger.error(f"session: {self.session_id} - error removing temporary file: {e}")
-
+            logger.error(f"session: {self._session_id} - error removing temporary file: {e}")
 
     async def get_metadata(self):
         if self._metadata:
             return self._metadata
 
         if not os.path.exists(self._output_file_path):
-            logger.warning(f"session: {self.session_id} - no metadata file found, creating new one")
+            logger.warning(f"session: {self._session_id} - no metadata file found, creating new one")
             return None
 
         try:
             container = av.open(self._output_file_path, mode="r")
         except Exception as e:
-            logger.error(f"session: {self.session_id} - failed to open video for metadata: {e}")
+            logger.error(f"session: {self._session_id} - failed to open video for metadata: {e}")
             return None
         video_stream = next((s for s in container.streams if s.type == "video"), None)
 
         if not video_stream:
-            logger.error(f"session: {self.session_id} - no video stream found in file")
+            logger.error(f"session: {self._session_id} - no video stream found in file")
             return None
 
         if video_stream.duration is not None and video_stream.time_base is not None:
@@ -111,8 +130,6 @@ class FrameCollector(BaseFrameCollector):
                 duration = None
 
         file_size = os.path.getsize(self.output_file_path)
-
-        mime_type = "video/mp4"
 
         if video_stream.average_rate:
             avg_fps = float(video_stream.average_rate)
@@ -129,7 +146,7 @@ class FrameCollector(BaseFrameCollector):
             "frame_count": video_stream.frames if video_stream.frames else None,
             "fps": avg_fps,
             "bit_rate": video_stream.bit_rate if video_stream.bit_rate else None,
-            "mime_type": mime_type
+            "mime_type": self.MIME_TYPE
         }
         container.close()
 
